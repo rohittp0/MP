39e219238f318065aff451d5f4ee4be1c190dc0c
Rohit T P <tprohit9@gmail.com>
2022-11-02 01:50:34 +0530

    ugly

--- source.asm
+++ source.asm
@@ -1,388 +1,360 @@
-section .data
-    command1 db " Enter a number from 1-9 " ,10
-    command1_len equ $-command1
+.MODEL SMALL
+.STACK 100h
 
-    command2 db " Congratulations you won " ,10
-    command2_len equ $-command2
+data segment
 
-    command3 db "Sorry the position is already taken" ,10
-    command3_len equ $-command3
+    number_prompt db " Enter a number from 1-9 " ,10
+    game_won db " Congratulations you won " ,10
+    error_message db "Sorry the position is already taken" ,10
 
-    next_line db 10
+    array db 10 dup('_')
+    input resq 1
+    player resq 'X', 'O'
 
-section .text
-  global _start
+data ends
 
-       push_x:
-        pop r15
+code segment
+assume cs:code, ds:data
 
-        push rax
-        push rdx
-        push rcx
-        push rdi
-        push rsi
-        push r14
+global start
 
-        push r15
+    push_x:
+        pop cx
+
+        push ax
+        push dx
+        push di
+        push cx
         ret
 
     pop_x:
-        pop r15
+        pop cx
 
-        pop r14
-        pop rsi
-        pop rdi
-        pop rcx
-        pop rdx
-        pop rax
+        pop si
+        pop di
+        pop dx
+        pop ax
 
-        push r15
+        push cx
         ret
 
-    print_command1:
-        mov rax,1
-        mov rdi,1
-        mov rsi,command1
-        mov rdx,command1_len
-        syscall
+    ask_number:
+        call push_x
+        lea dx,number_prompt  
+        mov ah,26 ; length of number_prompt
+        int 21h 
+        call pop_x
         ret
 
     print_won:
-        mov rax,1
-        mov rdi,1
-        mov rsi,command2
-        mov rdx,command2_len
-        syscall
+        call push_x
+        lea dx,game_won  
+        mov ah,26 ; length of game_won
+        int 21h 
+        call pop_x
         call exit
 
     print_error:
-        mov rax,1
-        mov rdi,1
-        mov rsi,command3
-        mov rdx,command3_len
-        syscall
+        call push_x
+        lea dx,error_message  
+        mov ah,36 ; length of error_message
+        int 21h 
+        call pop_x
+        
         jmp play_game
 
 
      print_enter:
         call push_x
-        mov rax,1
-        mov rdi,1
-        mov rsi,next_line
-        mov rdx,1
-        syscall
+        mov ah,2
+        mov dl,0ah
+        int 21h
+        mov dl,0dh
+        int 21h
         call pop_x
         jmp loop_exit
 
-    draw_grid:
-        mov r9,0
-        draw:
-        mov r8,'_'
-        mov [array+r9],r8
-        add r9,8
-        cmp r9,64
-        jle draw
-
-        mov r8, 'X'
-        mov [player], r8
-        mov r8, 'O'
-        mov [player+8], r8
-
-        ret
-
     read_input:
+        mov ah,1
+        int 21h
+        mov input, al
 
-        mov rax,0
-        mov rdi,0
-        mov rsi,input
-        mov rdx,1
-        syscall
+        mov ah,1
+        int 21h
 
-        mov rax,0
-        mov rdi,0
-        mov rsi,delim
-        mov rdx,1
-        syscall
+        mov bl,[input]
+        sub al,'1'
+        mul 8
 
-        mov r8,[input]
-        mov r10,[input]
-        sub r8,'1'
-        mov rax,r8
-        mov r8,8
-        mul r8
+        push ax
+        mov ax,cl
+        mov ch,2
+        div ch
+        mov ax, dx
+        mov ch, 8
+        mul ch
 
-        push rax
-        mov rax,r14
-        mov r15,2
-        div r15
-        mov rax, rdx
-        mov r15, 8
-        mul r15
+        mov ch, ax
+        pop ax
 
-        mov r15, rax
-        pop rax
-
-        mov r8, [player+r15]
-        mov r9,[array+rax]
-        cmp r9,'X'
+        mov ah, [player+ch]
+        mov al,[array+ax]
+        cmp al,'X'
+        
         je print_error
-        cmp r9,'O'
+        cmp al,'O'
         je print_error
-        mov [array+rax], r8
+        mov [array+ax], ah
 
         ret
 
     print_grid:
-        mov r8,array
-        mov r9,0
+        mov ah,array
+        mov al,0
 
         print_grid_loop:
-            mov rax,1
-            mov rdi,1
-            mov rsi,r8
-            mov rdx,1
-            syscall
+            lea dx,ah
+            mov ah,36 ; length of game_won
+            int 21h
 
-
-            mov rax,r9
-            mov r13,3
-            div r13
-            cmp rdx,0
+            mov ax,al
+            mov bh,3
+            div bh
+            cmp dx,0
             je print_enter
 
         loop_exit:
-            add r8,8
-            add r9,1
-            cmp r9,8
+            add ah,8
+            add al,1
+            cmp al,8
             jle print_grid_loop
             ret
 
         check:
-            mov rax,r14
-            mov r8,2
-            div r8
-            cmp rdx,0
+            mov ax,cl
+            mov ah,2
+            div ah
+            cmp dx,0
             je check_for_X
 
             jmp check_for_O
 
         check_for_X:
-            mov r8,[array+0]
-            cmp r8,'X'
+            mov ah,[array+0]
+            cmp ah,'X'
             je first_pos_check_X
 
-            mov r8,[array+32]
-            cmp r8,'X'
+            mov ah,[array+32]
+            cmp ah,'X'
             je second_pos_check_X
 
-            mov r8,[array+64]
-            cmp r8,'X'
+            mov ah,[array+64]
+            cmp ah,'X'
             je third_pos_check_X
 
             jmp check_exit
 
         first_pos_check_X:
-            mov r8,[array+8]
-            cmp r8,'X'
+            mov ah,[array+8]
+            cmp ah,'X'
             je X_win_1
 
-            mov r8,[array+32]
-            cmp r8,'X'
+            mov ah,[array+32]
+            cmp ah,'X'
             je X_win_2
 
-            mov r8,[array+24]
-            cmp r8,'X'
+            mov ah,[array+24]
+            cmp ah,'X'
             je X_win_3
 
             jmp check_exit
 
         second_pos_check_X:
-            mov r8,[array+8]
-            cmp r8,'X'
+            mov ah,[array+8]
+            cmp ah,'X'
             je X_win_4
 
-            mov r8,[array+24]
-            cmp r8, 'X'
+            mov ah,[array+24]
+            cmp ah, 'X'
             je X_win_5
 
-            mov r8,[array+16]
-            cmp r8, 'X'
+            mov ah,[array+16]
+            cmp ah, 'X'
             je X_win_3
 
             jmp check_exit
 
         third_pos_check_X:
-            mov r8,[array+40]
-            cmp r8, 'X'
+            mov ah,[array+40]
+            cmp ah, 'X'
             je X_win_6
 
-            mov r8,[array+56]
-            cmp r8,'X'
+            mov ah,[array+56]
+            cmp ah,'X'
             je X_win_7
 
             jmp check_exit
 
         X_win_1:
-            mov r8,[array+16]
-            cmp r8,'X'
+            mov ah,[array+16]
+            cmp ah,'X'
             je print_won
 
             jmp check_exit
 
         X_win_2:
-            mov r8,[array+64]
-            cmp r8, 'X'
+            mov ah,[array+64]
+            cmp ah, 'X'
             je print_won
 
             jmp check_exit
 
         X_win_3:
-            mov r8,[array+48]
-            cmp r8, 'X'
+            mov ah,[array+48]
+            cmp ah, 'X'
             je print_won
 
             jmp check_exit
 
-         X_win_4:
-            mov r8,[array+56]
-            cmp r8, 'X'
+        X_win_4:
+            mov ah,[array+56]
+            cmp ah, 'X'
             je print_won
 
             jmp check_exit
 
-         X_win_5:
-            mov r8,[array+40]
-            cmp r8,'X'
+        X_win_5:
+            mov ah,[array+40]
+            cmp ah,'X'
             je print_won
 
             jmp check_exit
 
-         X_win_6:
-            mov r8,[array+16]
-            cmp r8, 'X'
+        X_win_6:
+            mov ah,[array+16]
+            cmp ah, 'X'
             je print_won
 
             jmp check_exit
 
-         X_win_7:
-            mov r8,[array+48]
-            cmp r8, 'X'
+        X_win_7:
+            mov ah,[array+48]
+            cmp ah, 'X'
             je print_won
 
             jmp check_exit
 
-         check_for_O:
-            mov r8,[array+0]
-            cmp r8,'O'
+        check_for_O:
+            mov ah,[array+0]
+            cmp ah,'O'
             je first_pos_check_O
 
-            mov r8,[array+32]
-            cmp r8,'O'
+            mov ah,[array+32]
+            cmp ah,'O'
             je second_pos_check_O
 
-            mov r8,[array+64]
-            cmp r8,'O'
+            mov ah,[array+64]
+            cmp ah,'O'
             je third_pos_check_O
 
             jmp check_exit
 
-         first_pos_check_O:
-             mov r8,[array+8]
-             cmp r8,'O'
+        first_pos_check_O:
+             mov ah,[array+8]
+             cmp ah,'O'
              je O_win_1
 
-             mov r8,[array+32]
-             cmp r8,'O'
+             mov ah,[array+32]
+             cmp ah,'O'
              je O_win_2
 
-             mov r8,[array+24]
-             cmp r8,'O'
+             mov ah,[array+24]
+             cmp ah,'O'
              je O_win_3
 
              jmp check_exit
 
-         second_pos_check_O:
-             mov r8,[array+8]
-             cmp r8,'O'
+        second_pos_check_O:
+             mov ah,[array+8]
+             cmp ah,'O'
              je O_win_4
 
-             mov r8,[array+24]
-             cmp r8, 'O'
+             mov ah,[array+24]
+             cmp ah, 'O'
              je O_win_5
 
-             mov r8,[array+16]
-             cmp r8, 'O'
+             mov ah,[array+16]
+             cmp ah, 'O'
              je O_win_3
 
              jmp check_exit
 
-         third_pos_check_O:
-             mov r8,[array+40]
-             cmp r8, 'O'
+        third_pos_check_O:
+             mov ah,[array+40]
+             cmp ah, 'O'
              je O_win_6
 
-             mov r8,[array+56]
-             cmp r8,'O'
+             mov ah,[array+56]
+             cmp ah,'O'
              je O_win_7
 
              jmp check_exit
 
-          O_win_1:
-             mov r8,[array+16]
-             cmp r8,'O'
+        O_win_1:
+             mov ah,[array+16]
+             cmp ah,'O'
              je print_won
 
             jmp check_exit
 
-          O_win_2:
-             mov r8,[array+64]
-             cmp r8, 'O'
+        O_win_2:
+             mov ah,[array+64]
+             cmp ah, 'O'
              je print_won
 
              jmp check_exit
 
-          O_win_3:
-              mov r8,[array+48]
-              cmp r8, 'O'
+        O_win_3:
+              mov ah,[array+48]
+              cmp ah, 'O'
               je print_won
 
               jmp check_exit
 
-          O_win_4:
-               mov r8,[array+56]
-               cmp r8, 'O'
+        O_win_4:
+               mov ah,[array+56]
+               cmp ah, 'O'
                je print_won
 
                jmp check_exit
 
-           O_win_5:
-               mov r8,[array+40]
-               cmp r8,'O'
+        O_win_5:
+               mov ah,[array+40]
+               cmp ah,'O'
                je print_won
 
                jmp check_exit
 
-           O_win_6:
-               mov r8,[array+16]
-               cmp r8, 'O'
+        O_win_6:
+               mov ah,[array+16]
+               cmp ah, 'O'
                je print_won
 
                jmp check_exit
 
-            O_win_7:
-               mov r8,[array+48]
-               cmp r8, 'O'
+        O_win_7:
+               mov ah,[array+48]
+               cmp ah, 'O'
                je print_won
 
                jmp check_exit
 
-    _start:
-       mov r14,0
-       call draw_grid
+    start:
+       mov cl,0
 
         play_game:
 
-            call print_command1
+            call ask_number
             call read_input
 
 
@@ -391,22 +363,17 @@ section .text
                 call check
 
             check_exit:
-                add r14,1
-                cmp r14,8
+                add cl,1
+                cmp cl,8
                 jle play_game
     call check
     call exit
 
 
     exit:
-        mov rax,60
-        mov rdx,0
-        syscall
+        mov ah, 4ch
+        int 21h
 
+code ends
 
-    section .bss
-        array resq 9
-        input resq 1
-        delim resq 1
-        player resq 2
 
